2d387927eda5601dcb41863bdb8232c415b31e1d
TeymurQurbanov99 <teyqur@gmail.com>
2022-11-17 16:53:23 +0400

    starting making a pizza website

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../assets/pizzalogo.jpg"
+
+function Navbar() {
+    return(
+        <div className="navbar">
+            <div className="leftSide"> 
+             <img src={Logo}/>
+             </div>
+            <div className="rightSide"> </div>
+        </div>
+    );
+}
+
+export default Navbar;
