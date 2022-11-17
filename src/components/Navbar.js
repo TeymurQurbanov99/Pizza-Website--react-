8157b6a226a3389d3a88da547c1cac27b2e2aa1b
TeymurQurbanov99 <teyqur@gmail.com>
2022-11-17 17:36:02 +0400

    final version, a few pages  - home, menu, about, contact

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Logo from "../assets/pizzaLogo.png";
 import { Link } from "react-router-dom";
-import ReorderIcon from "@material-ui/icons/Reorder";
+
 import "../styles/Navbar.css";
 
 function Navbar() {
@@ -15,19 +15,19 @@ function Navbar() {
       <div className="leftSide" id={openLinks ? "open" : "close"}>
         <img src={Logo} />
         <div className="hiddenLinks">
-          <Link to="/"> Home </Link>
+          <Link to="/home"> Home </Link>
           <Link to="/menu"> Menu </Link>
           <Link to="/about"> About </Link>
           <Link to="/contact"> Contact </Link>
         </div>
       </div>
       <div className="rightSide">
-        <Link to="/"> Home </Link>
+        <Link to="/home"> Home </Link>
         <Link to="/menu"> Menu </Link>
         <Link to="/about"> About </Link>
         <Link to="/contact"> Contact </Link>
         <button onClick={toggleNavbar}>
-          <ReorderIcon />
+          
         </button>
       </div>
     </div>
--- src/pages/About.js
+++ src/pages/About.js
@@ -11,7 +11,7 @@ function About() {
       <div className="aboutBottom">
         <h1> ABOUT US</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          Hello, my name is Teymur and this is my test project. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
           mollitia, molestiae quas vel sint commodi repudiandae consequuntur
           voluptatum laborum numquam blanditiis harum quisquam eius sed odit
           fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
